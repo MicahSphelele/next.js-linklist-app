4c4dcbc51bf1688da331bc6805669756218f59dd
Sphelele-Micah-Ngubane <sphemicah@gmail.com>
2024-06-23 13:15:09 +0200

    Added new field on Page schema  💻

--- src/domain/models/db/page.ts
+++ src/domain/models/db/page.ts
@@ -1,18 +1,21 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const pageSchema = new Schema({
-    uri : { type: String, required: true, min: 1, unique: true },
+const pageSchema = new Schema(
+  {
+    uri: { type: String, required: true, min: 1, unique: true },
     owner: { type: String, required: true, min: 1 },
-    displayName: { type: String, default: ""},
-    location: { type: String, default: ""},
-    bio: { type: String, default: ""},
-    bgType: { type: String, default: "color"},
-    bgColor: { type: String, default: "#000000"},
+    displayName: { type: String, default: "" },
+    location: { type: String, default: "" },
+    bio: { type: String, default: "" },
+    bgType: { type: String, default: "color" },
+    bgColor: { type: String, default: "#000000" },
     bgImageUrl: { type: String, default: "" },
     bgImageKey: { type: String, default: "" },
     avater: { type: String, default: "" },
-    avaterKey: { type: String, default: "" }
-
-}, { timestamps: true, versionKey: false });
+    avaterKey: { type: String, default: "" },
+    buttons: { type: Object, default: {} },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 export const Page = models?.Page || model("Page", pageSchema);
